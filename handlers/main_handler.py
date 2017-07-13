4639e852d2772f7b66a0c9c64e3f6970b801d578
Shaylah0701 <shaylah0701@gmail.com>
2017-07-13 12:01:54 -0400

    Creating main page: menu, title, etc

--- handlers/main_handler.py
+++ handlers/main_handler.py
@@ -7,7 +7,7 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
     	logging.info("MainHandler")
         html_params = {
-            "title": "Main Title",
+            "title": "Hampton HangOuts",
             "content": "Hello"
         }
         template = jinja_env.env.get_template('templates/tmpl.html')
