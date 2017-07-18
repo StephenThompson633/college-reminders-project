280080275140599a74a9064a27c657f330bbed95
Shaylah0701 <shaylah0701@gmail.com>
2017-07-18 10:07:29 -0400

    more shit

--- handlers/event_handler.py
+++ handlers/event_handler.py
@@ -1,1 +1,15 @@
-event_handler.py
+import jinja_env
+import logging
+import webapp2
+from google.appengine.api import users
+
+class EventHandler(webapp2.RequestHandler):
+    def get(self):
+        logging.info("EventHandler")
+        html_params = {
+            "title": "My Reminders",
+            "content": "Hello",
+            }
+
+        template = jinja_env.env.get_template('templates/event.html')
+        self.response.out.write(template.render(html_params))
