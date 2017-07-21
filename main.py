bb7e244bb5f3c21025ebb1c1d5089ceb62b42df5
Shaylah0701 <shaylah0701@gmail.com>
2017-07-21 08:40:18 -0400

    f

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from handlers import jinja_env
 from handlers import main_handler
 from handlers import form_handler
 from handlers import remind_handler
-from handlers import map_handler
+
 from handlers import event_handler
 
 jinja_env.init(os.path.dirname(__file__))
@@ -19,7 +19,8 @@ app = webapp2.WSGIApplication([
     ('/', main_handler.MainHandler),
     ('/QA', form_handler.FormHandler),
     ('/remind', remind_handler.RemindHandler),
-    ('/map', map_handler.MapHandler),
+
     ('/event', event_handler.EventHandler),
+
 
 ], debug=True)
